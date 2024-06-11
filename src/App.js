5c13495240572cf48eb7b899bd7a3af24eae8eb6
SANDRA-S-MENON <sandramenon2002@gmail.com>
2024-06-11 20:52:52 +0530

    view created

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import AddCourse from './components/AddCourse';
+import ViewAll from './components/ViewAll';
 
 function App() {
   return (
     <div>
       <AddCourse/>
+      <ViewAll/>
      
     </div>
   );
---
+++ src/components/ViewAll.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import NavBar from './NavBar'
+
+const ViewAll = () => {
+    return (
+        <div>
+            <NavBar/>
+            <div className="container">
+                <div className="row">
+                    <div className="col col-12 col-sm-12 col-md-12 col-lg-12 col-xl-12 col-xxl-12">
+                        <table class="table" border="3">
+                            <thead>
+                                <tr>
+                                    <th scope="col">#</th>
+                                    <th scope="col">First</th>
+                                    <th scope="col">Last</th>
+                                    <th scope="col">Handle</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <th scope="row">1</th>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                </tr>
+                                
+
+
+
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default ViewAll
